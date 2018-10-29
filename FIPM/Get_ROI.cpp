b21f2797ba38ae5b373d9dcb2133180ccc1a2b19
edel1 <ghygg@naver.com>
2018-10-29 18:41:23 +0900

    test1

--- FIPM/Get_ROI.cpp
+++ FIPM/Get_ROI.cpp
@@ -1,1 +1,27 @@
+#include "pch.h"
 #include "Get_ROI.h"
+
+
+
+void GetImg(std::string path)
+{
+	cv::Mat img = cv::imread(path, 0);
+	GetROI(img);
+	//cv::imshow("img", img);
+	//cv::waitKey();
+
+}
+
+void GetROI(cv::Mat img)
+{
+	//ROI 추출 후 HAT 씌우기........
+
+	//1. 경계선 추출
+	//2. 사각형 그리기.
+	//3. 이쁘게
+}
+
+void DisplayROI()
+{
+
+}
--- FIPM/Get_ROI.h
+++ FIPM/Get_ROI.h
@@ -1,1 +1,11 @@
 #pragma once
+#include <opencv2\opencv.hpp>
+
+
+
+void GetImg(std::string path);
+
+void GetROI(cv::Mat img);
+
+void DisplayROI();
+
